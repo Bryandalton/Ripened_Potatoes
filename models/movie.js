e6e63acda765bbf49c35702de1f7b1e05db67b3c
[Bryan Dalton] <[bryantdalton19@gmail.com]>
2022-11-21 19:11:29 -0500

    created movies model

--- models/movie.js
+++ models/movie.js
@@ -1,2 +1,43 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Movie extends Model {}
+
+Movie.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  tag_id: {
+    type: DataTypes.INTEGER,
+    references: {
+        model: 'tag',
+        key: 'id',
+    }
+  },
+  review_id: {
+    type: DataTypes.INTEGER,
+    references: {
+        model: 'review',
+        key: 'id',
+    }
+  }
+},
+{
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'movie',
+  }
+);
