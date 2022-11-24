f36c9be57d069f9307a01b826b40f8204ba1f977
[Bryan Dalton] <[bryantdalton19@gmail.com]>
2022-11-23 19:08:03 -0500

    added profile and login routes to homeroutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,53 +1,79 @@
-const router = require('express').Router();
-const { User, Review, Movie, Tag } = require('../models');
-const withAuth = require('../utils/auth');
-const { move } = require('./api');
+const router = require("express").Router();
+const { User, Review, Movie, Tag } = require("../models");
+const withAuth = require("../utils/auth");
+// const { move } = require("./api");
 
-router.get('/', async (req, res) => { 
-    try{
-        const movieData = await Movie.findAll({
-            include: [
-                {
-                    model: Review,
-                    attributes: ['description', 'rating']
-                },
-                {
-                    model: Tag,
-                    attributes: ['tag_name']
-                },
-            ],
-        });
-        const movies = movieData.map((movie) => movie.get({ plain: true }));
-        res.render('homePage', {
-            movies,
-            //login?
-        });
-    } catch(err) {
-        res.status(500).json(err);
-    }
-});
-
-router.get('/movie/:id', async (req, res) => {
-    try{
-        const movieData = await Movie.findByPk(req.params.id,{
-            include: [
-                {
-                    model: Review,
-                    attributes: ['description', 'rating']
-                },
-                {
-                    model: Tag,
-                    attributes: ['tag_name']
-                },
-            ],
-        });
-        const movie = movieData.get({plain: true});
-        res.render('movie', {
-            ...movie,
-            logged_in: req.session.logged_in
-        });
-    }
-    catch(err){
+router.get("/", async (req, res) => {
+  try {
+    const movieData = await Movie.findAll({
+      include: [
+        {
+          model: Review,
+          attributes: ["description", "rating"],
+        },
+        {
+          model: Tag,
+          attributes: ["tag_name"],
+        },
+      ],
+    });
+    const movies = movieData.map((movie) => movie.get({ plain: true }));
+    res.render("homePage", {
+      movies,
+      //login?
+    });
+  } catch (err) {
     res.status(500).json(err);
-    }
+  }
 });
+
+router.get("/movie/:id", async (req, res) => {
+  try {
+    const movieData = await Movie.findByPk(req.params.id, {
+      include: [
+        {
+          model: Review,
+          attributes: ["description", "rating"],
+        },
+        {
+          model: Tag,
+          attributes: ["tag_name"],
+        },
+      ],
+    });
+    const movie = movieData.get({ plain: true });
+    res.render("movie", {
+      ...movie,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/profile", withAuth, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+      include: [{ model: Review }],
+    });
+    const user = userData.get({ plain: true });
+    res.render("profile", {
+      ...user,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/login", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/profile");
+    return;
+  }
+
+  res.render("login");
+});
+
+module.exports = router;
