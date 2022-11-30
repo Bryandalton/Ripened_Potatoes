853a380a4a716a6b838e411db9c417a2268c98ea
[Bryan Dalton] <[bryantdalton19@gmail.com]>
2022-11-30 15:51:34 -0500

    database seeds

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,7 +1,27 @@
-const sequelize = require('../config/connection');
-const { User, Movie, Review, Tag } = require('../models');
+const sequelize = require("../config/connection");
+const { User, Movie, Review, Tag } = require("../models");
 
-const userData = require('./');
-const movieData = require();
-const reviewData = require();
-const tagData = require();
+const userData = require("./userData.json");
+const movieData = require("./movieData.json");
+// const reviewData = require();
+const tagData = require("./tagData.json");
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+  await Tag.bulkCreate(tagData, {
+    individualHooks: true,
+    returning: true,
+  });
+  await Movie.bulkCreate(movieData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  process.exit(0);
+};
+
+seedDatabase();
